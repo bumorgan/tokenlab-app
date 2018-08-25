a0e25b4a92e5ecbc89429e865d6bd3b5b3eaa3de
Bruno Morgan <bumorgan@hotmail.com>
2018-08-25 14:37:54 -0300

    Retrofit implemented with success Get data from url of games (json) OK!

--- app/src/main/java/tokenlabapp/bm/com/tokenlabapp/MainActivity.java
+++ app/src/main/java/tokenlabapp/bm/com/tokenlabapp/MainActivity.java
@@ -2,6 +2,16 @@ package tokenlabapp.bm.com.tokenlabapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        Call<GameList> call = api.getGames();
+
+        call.enqueue(new Callback<GameList>() {
+            @Override
+            public void onResponse(Call<GameList> call, Response<GameList> response) {
+                GameList games = response.body();
+                for(Game g: games.getGames()) {
+                    Log.d("id", g.getId());
+                    Log.d("name", g.getName());
+                    Log.d("image", g.getImage());
+                    Log.d("release_date", g.getRelease_date());
+                    Log.d("trailer", g.getTrailer());
+                    Log.d("platforms", g.getPlatforms().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GameList> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
